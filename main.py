1f7a39c5f67161cfac146b3021739dba23d8d2b4
Ganesh R <kdark8276@gmail.com>
2026-06-13 11:58:22 +0530

    main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ percentage_difference = round(
     (difference / float(day_before_yesterday_closing_date)) * 100
 )
 
-if abs(percentage_difference) > 1:
+if abs(percentage_difference) > 0:
 
     news_parameter = {
         "apikey": news_API_KEY,
